c7b8789a1245a4a81bdb68f9444d052efaea90a8
Hatshepsut <amabelle.delacruz322@gmail.com>
2018-11-10 10:09:40 -0800

    added some js to file

---
+++ assets/javascript/game-word.js
@@ -0,0 +1,23 @@
+// theme - Rick and Morty
+
+// answer: BirdPerson
+var answer = "Birdperson";
+
+// User presses any key to initialize
+
+// variables on page
+var wins = 0;
+var loss = 0;
+var guessLeft = 0;
+var guessFar = 0;
+
+// function for user choice
+document.onkeyup = function(event) {
+    var userInput = event.key;
+}
+// letters that can be used
+var letters = ["A", "B", "C", "D", "E", "F", "G", "H", "I", "J", "K", "L", "M", "N", "O", "P", "Q", "R", "S", "T", "U", "V", "W", "X", "Y", "Z", "_"];
+
+
+
+
